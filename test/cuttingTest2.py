f6ec3ae9b48ae03867369e6248d3be9fb1877411
MidgeStone <mail@pierremigeot.com>
2019-10-13 23:27:19 +0200

    add main file: working weel. (in my case, better than with the module ffmpeg)

--- test/cuttingTest2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-start = "00:00:00"
-end = "00:00:10"
-cmd = ["ffmpeg", "-i", "totoro.mp4", "-ss", start, "-to", end, "-c", "copy", "totoro2.mp4"]
-subprocess.run(cmd, stderr=subprocess.STDOUT)
